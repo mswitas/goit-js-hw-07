cc06358c3389941c1cc1b433db734f87e28bbbc0
mswitas <mswitas@gmail.com>
2023-10-17 14:55:20 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let galleryItemsString = "";
+
+galleryItems.forEach((galleryItem) => {
+    galleryItemsString += `
+        <li>
+            <div class="gallery__item">
+                <a class="gallery__link" href="${galleryItem.original}">
+                    <img
+                        class="gallery__image"
+                        src="${galleryItem.preview}"
+                        data-source="${galleryItem.original}"
+                        alt="${galleryItem.description}"
+                    />
+                </a>
+            </div>
+        </li>    
+    `;
+
+});
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("afterbegin", galleryItemsString);
